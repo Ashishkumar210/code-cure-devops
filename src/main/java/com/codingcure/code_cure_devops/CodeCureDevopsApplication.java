ec07d1dfbbfa0b50425a657a0f97cd32588c397e
ashish <ashishmahanta121@gmail.com>
2025-09-27 00:41:17 +0530

    first commit

---
+++ src/main/java/com/codingcure/code_cure_devops/CodeCureDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.codingcure.code_cure_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCureDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeCureDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingcure/code_cure_devops/CodeCureDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingcure.code_cure_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCureDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
